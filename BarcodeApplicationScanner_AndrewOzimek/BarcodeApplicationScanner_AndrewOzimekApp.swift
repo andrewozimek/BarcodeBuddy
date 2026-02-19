5589eb213931952636a6ec721d8cee16359d4ac5
Andrew Ozimek <andrewozimek@hotmail.com>
2026-02-18 19:51:50 -0500

    Initial Commit

---
+++ BarcodeApplicationScanner_AndrewOzimek/BarcodeApplicationScanner_AndrewOzimekApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeApplicationScanner_AndrewOzimekApp.swift
+//  BarcodeApplicationScanner_AndrewOzimek
+//
+//  Created by Ozimek, Andrew R. on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeApplicationScanner_AndrewOzimekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
